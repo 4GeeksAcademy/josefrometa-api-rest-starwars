f839654b52293ccc5d0a9cd203fe0e50103516b8
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-10-31 02:58:06 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -7,6 +7,29 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_favorite = db.relationship("Favorite", uselist=True, backref="user")
+
+    
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(50), unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    planets_favorite = db.relationship("Favorite", uselist=True, backref="planets")
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(50), unique=False, nullable=False)
+    character_age = db.Column(db.Integer, unique=False, nullable=True)
+    characters_favorite = db.relationship("Favorite", uselist=True, backref="characters")
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +40,5 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+    
